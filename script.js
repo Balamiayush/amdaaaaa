9987d24b037f4f1776de3998251eeb71003b77e5
Balamiayush <aryanbalami54@gmail.com>
2025-04-22 19:10:25 +0545

    Add Lenis smooth scrolling library and implement scroll event logging

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
     initializeSearchFunctionality();
     injectHamburgerCSS();
 
-    
+    const lenis = new Lenis({
+        autoRaf: true,
+      });
+      
+      // Listen for the scroll event and log the event data
+      lenis.on('scroll', (e) => {
+        console.log(e);
+      });
    
 });
 
